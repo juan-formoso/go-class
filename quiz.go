ef393f01dd41d4670ab63b588a566b5c94a866c0
Juan Formoso <trajano.juan@gmail.com>
2021-12-18 21:39:55 -0400

    Add first question and increase input

--- quiz.go
+++ quiz.go
@@ -5,18 +5,33 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to my quiz game!")
 
-	// VARIABLES AND DATA TYPES
-	// string => everything in a double quotation mark
-	// int => whole numbers
-	// uint => whole numbers with no negative numbers
-	// float64 => decimal numbers
-	// bool => true or false
-	// name := "John" | short way
-	// %v is a placeholder for the variable
-	// %d does the same but for decimal numbers
-	// PRINT AND SCAN
-	// Scan let the user input data
-	// \n is a new line
+	/*
+		VARIABLES AND DATA TYPES
+		string => everything in a double quotation mark
+		int => whole numbers
+		uint => whole numbers with no negative numbers
+		float64 => decimal numbers
+		bool => true or false
+		name := "John" | short way
+		%v is a placeholder for the variable
+		%d does the same but for decimal numbers
+
+		PRINT AND SCAN
+		Scan let the user input data
+		\n is a new line
+
+		CONDITIONS
+		!= is not equal to
+		== is equal to
+		< is less than
+		> is greater than
+		<= is less than or equal to
+		>= is greater than or equal to
+
+		CONDITIONALS
+		if statement
+		if else statement
+	*/
 
 	fmt.Printf("Enter\n your name: ")
 	var name string
@@ -27,4 +42,17 @@ func main() {
 	fmt.Printf("Enter your age: ")
 	var age uint
 	fmt.Scan(&age)
+
+	if age >= 10 {
+		fmt.Println("Yay, you are old enough to play! :D")
+	} else {
+		fmt.Println("Ops! You are not old enough to play! :(")
+		return // this will stop the program
+	}
+
+	fmt.Printf("What is better, a dog or a cat? ")
+	var answer string
+	fmt.Scan(&answer)
+
+	fmt.Println(answer)
 }
