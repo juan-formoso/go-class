c44cd2764850014105f54b945d77ed5ff7e71dda
Juan Formoso <trajano.juan@gmail.com>
2021-12-18 21:23:35 -0400

    Add print and scan

--- quiz.go
+++ quiz.go
@@ -5,14 +5,26 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to my quiz game!")
 
-	// Variables and Data Types
+	// VARIABLES AND DATA TYPES
 	// string => everything in a double quotation mark
 	// int => whole numbers
 	// uint => whole numbers with no negative numbers
 	// float64 => decimal numbers
 	// bool => true or false
-	// var name string = "John" | long way
+	// name := "John" | short way
+	// %v is a placeholder for the variable
+	// %d does the same but for decimal numbers
+	// PRINT AND SCAN
+	// Scan let the user input data
+	// \n is a new line
 
-	name := "John"
-	fmt.Println(name)
+	fmt.Printf("Enter\n your name: ")
+	var name string
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello %v, welcome to the game!", name)
+
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
 }
