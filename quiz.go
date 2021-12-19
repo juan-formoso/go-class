bac5c2b69d1dc1c6c54024a9ec3810b62e560dae
Juan Formoso <trajano.juan@gmail.com>
2021-12-18 20:55:11 -0400

    Add Variables and Data Types notes

---
+++ quiz.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to my quiz game!")
+
+	// Variables and Data Types
+	// string => everything in a double quotation mark
+	// int => whole numbers
+	// uint => whole numbers with no negative numbers
+	// float64 => decimal numbers
+	// bool => true or false
+	// var name string = "John" | long way
+
+	name := "John"
+	fmt.Println(name)
+}
